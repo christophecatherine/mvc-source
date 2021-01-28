f709c8ba8d620c6f0fdf09afec37bf3b0351142d
christophecatherine <1christophecatherine@gmail.com>
2021-01-28 16:24:58 +0100

    mvc7

---
+++ app.js
@@ -0,0 +1,33 @@
+// Allez chercher express 
+const express = require('express')
+const exphbs = require('express-handlebars');
+
+
+// Notre app va chercher express
+const app = express();
+
+// Notre app utile express static pour les dossier 'public'
+app.use(express.static('public'));
+
+//Route 
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//demande vers index
+app.get("/", function(req, res) {
+
+    res.render("index")
+})
+
+//demande vers contact
+app.get("/contact", function(req, res) {
+    res.render("contact")
+})
+
+//ecoute vers le port 3000
+app.listen(3000, function() {
+    console.log("Le serveur tourne sur le port 3000");
+
+})
